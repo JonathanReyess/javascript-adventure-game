d601a2a659b2e853e8ee136bbbd3fe475a20a794
Jonathan Reyes <95444170+JonathanReyess@users.noreply.github.com>
2021-12-13 15:57:44 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,25 @@
-const textElement = document.getElementById('text')
-const optionButtonsElement = document.getElementById('option-buttons')
+let adventureDescriptionText = document.querySelector(".adventure-description");
+let optionButtons = document.getElementsByClassName("button");
 
-let state = {}
-
-function startGame() {
-  state = {}
-  showTextNode(1)
+function updateAdventureText() {
+  adventureDescriptionText.innerText = textAdventure[0].text;
 }
+
+function updateOptions() {
+  let i = 0;
+  for(i; i < optionButtons.length; i++) {
+    console.log(optionButtons[i]);
+    optionButtons[i].innerText = textAdventure[0].options[i];
+  }
+}
+
+const textAdventure = [
+  {
+    id: 1,
+    text: 'This is the adventure of Juan, who got lost in the forest when looking for a bird to photograph. Juan needs your help to find his way out. Choose one of the options below',
+    options: ["Head south and find the river" , "Go back to the bottom of the hill" ]
+  },
+]
+
+updateAdventureText();
+updateOptions();
